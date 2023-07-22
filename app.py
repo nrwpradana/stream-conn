b17c2bc88837d534432170a2bbe345cd8b29a051
Nadhiar Pradana <nadhiar11@gmail.com>
2023-07-22 20:59:59 +0700

    Edit API

--- app.py
+++ app.py
@@ -4,20 +4,25 @@ from connection import VirusTotalConnection
 
 st.title("VirusTotal API Test")
 
-# Replace 'YOUR_VIRUSTOTAL_API_KEY' with your actual VirusTotal API key
-API_KEY = '1578cfbb6b7c950b6a61c56ab5bc30087254b25773a4560eb6d361091541ecde'
-connection = VirusTotalConnection(API_KEY)
+# Input for user to enter the VirusTotal API key
+API_KEY = st.text_input("Enter your VirusTotal API key", type="password")
 
-# Test the connection with a SHA256 hash
-sha256_hash = "8c52bb3c67a0de4c0f1590057b2a864158e30aa9c717eaa01a32b1cf8ae9bbfd"
-file_report = connection.get_file_report(sha256_hash)
+# Check if the API key is provided
+if API_KEY:
+    connection = VirusTotalConnection(API_KEY)
 
-# Test the connection with a URL
-#url = "YOUR_URL"
-#url_report = connection.get_url_report(url)
+    # Test the connection with a SHA256 hash
+    sha256_hash = "8c52bb3c67a0de4c0f1590057b2a864158e30aa9c717eaa01a32b1cf8ae9bbfd"
+    file_report = connection.get_file_report(sha256_hash)
 
-st.subheader("File Report:")
-st.write(pd.DataFrame.from_dict(file_report))
+    # Test the connection with a URL
+    url = "YOUR_URL"
+    url_report = connection.get_url_report(url)
 
-#st.subheader("URL Report:")
-#st.write(pd.DataFrame.from_dict(url_report))
+    st.subheader("File Report:")
+    st.write(pd.DataFrame.from_dict(file_report))
+
+    st.subheader("URL Report:")
+    st.write(pd.DataFrame.from_dict(url_report))
+else:
+    st.warning("Please enter your VirusTotal API key.")
