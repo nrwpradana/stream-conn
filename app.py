7d903d2e08777543c8bfac2091170aa8b3644d78
Nadhiar Pradana <nadhiar11@gmail.com>
2023-07-22 15:39:59 +0700

    Kembali

--- app.py
+++ app.py
@@ -1,20 +1,24 @@
 import streamlit as st
-from streamlit.connections import ExperimentalBaseConnection
 import requests
 
-class EtherscanConnection(ExperimentalBaseConnection):
-    def _connect(self, api_key):
-        # Return the Etherscan API key as the connection object
-        return api_key
+# Define the Connection class for Etherscan API
+class EtherscanConnection:
+    def __init__(self, api_key):
+        self.api_key = api_key
+        self.base_url = "https://api.etherscan.io/api"
+
+    def _connect(self):
+        # No specific connection setup is required for Etherscan API
+        pass
 
     def cursor(self):
-        # Return the Etherscan API key (connection object) as the cursor
-        return self._instance
+        # Return the Etherscan API instance, which is just the base URL
+        return self.base_url
 
     @st.cache(allow_output_mutation=True)
     def query(self, module, action, address, tag="latest"):
         # Perform the API query and return the balance for the specified Ethereum address
-        url = f"https://api.etherscan.io/api?module={module}&action={action}&address={address}&tag={tag}&apikey={self.cursor()}"
+        url = f"{self.base_url}?module=account&action=balance&address={address}&tag=latest&apikey={self.api_key}"
 
         response = requests.get(url)
         data = response.json()
@@ -27,32 +31,37 @@ class EtherscanConnection(ExperimentalBaseConnection):
             st.error(data["message"])
             return None
 
+# Create an instance of the EtherscanConnection class using st.experimental_connection
+#@st.experimental_memo(backend="etherscan_connection")
+def create_connection():
+    api_key = st.text_input("Enter your Etherscan API key", type="password")
+    return EtherscanConnection(api_key)
+
 def main():
-    st.title("Etherscan Streamlit App")
+    st.title("Etherscan API Connection Demo")
 
-    # API key input
-    api_key = st.text_input("Enter your Etherscan API key:")
+    # Get the EtherscanConnection instance from the connection
+    connection = create_connection()
 
-    if not api_key:
-        st.warning("Please enter your Etherscan API key.")
-        st.stop()
+    if connection.api_key:
+        st.success("Etherscan API connected successfully!")
 
-    # Create a connection object
-    connection = EtherscanConnection(api_key)
+        # Demo Etherscan API functionality
+        st.header("Demo Etherscan API Functionality")
 
-    # Query form
-    st.subheader("Fetch Ethereum Balance")
-    address = st.text_input("Enter the Ethereum address:")
-
-    if st.button("Fetch"):
+        # Example query to retrieve Ethereum balance by address
+        st.text("Example Address : 0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae")
+        address = st.text_input("Enter an Ethereum address")
         if address:
             balance = connection.query(module="account", action="balance", address=address)
             if balance is not None:
-                st.write(f"Balance of {address}: {balance} wei")
-            else:
-                st.warning("Failed to fetch balance.")
-        else:
-            st.warning("Please enter an Ethereum address.")
+                st.write("Ethereum Address Balance:")
+                st.write(balance)
+
+    link_text = "Click here to visit Github"
+    link_url = "https://github.com/nrwpradana/stream-conn"
+    link_markdown = f"[{link_text}]({link_url})"
+    st.markdown(link_markdown, unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
