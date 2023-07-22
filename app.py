a3073d4894d45ca3e7ff0996339b0c5cb1619f35
Nadhiar Pradana <nadhiar11@gmail.com>
2023-07-22 20:56:26 +0700

    Edit app

--- app.py
+++ app.py
@@ -1,29 +1,23 @@
 import streamlit as st
+import pandas as pd
 from connection import VirusTotalConnection
-
-# Retrieve the VirusTotal API key from Streamlit secrets
-API_KEY = st.secrets["virustotal_api_key"]
-connection = VirusTotalConnection(API_KEY)
 
 st.title("VirusTotal API Test")
 
-# Input for user to enter SHA256 hash or URL
-input_type = st.radio("Select Input Type", ("SHA256 Hash", "URL"))
+# Replace 'YOUR_VIRUSTOTAL_API_KEY' with your actual VirusTotal API key
+API_KEY = '1578cfbb6b7c950b6a61c56ab5bc30087254b25773a4560eb6d361091541ecde'
+connection = VirusTotalConnection(API_KEY)
 
-if input_type == "SHA256 Hash":
-    sha256_hash = st.text_input("Enter SHA256 Hash")
-    if st.button("Get File Report"):
-        if sha256_hash:
-            report = connection.get_file_report(sha256_hash)
-            st.json(report)
-        else:
-            st.error("Please enter a valid SHA256 hash.")
+# Test the connection with a SHA256 hash
+sha256_hash = "8c52bb3c67a0de4c0f1590057b2a864158e30aa9c717eaa01a32b1cf8ae9bbfd"
+file_report = connection.get_file_report(sha256_hash)
 
-else:
-    url = st.text_input("Enter URL")
-    if st.button("Get URL Report"):
-        if url:
-            report = connection.get_url_report(url)
-            st.json(report)
-        else:
-            st.error("Please enter a valid URL.")
+# Test the connection with a URL
+#url = "YOUR_URL"
+#url_report = connection.get_url_report(url)
+
+st.subheader("File Report:")
+st.json(pd.json_normalize(file_report))
+
+#st.subheader("URL Report:")
+#st.json(pd.json_normalize(url_report))
