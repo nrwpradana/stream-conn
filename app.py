8947d40fece24eece00397109a276a7679ccf2c0
Nadhiar Pradana <nadhiar11@gmail.com>
2023-07-22 21:11:25 +0700

    Upload File

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import hashlib
 from connection import VirusTotalConnection
 
 st.title("VirusTotal API Test")
@@ -11,16 +12,23 @@ API_KEY = st.text_input("Enter your VirusTotal API key", type="password")
 if API_KEY:
     connection = VirusTotalConnection(API_KEY)
 
-    # Input for user to enter the SHA256 hash
-    sha256_hash = st.text_input("Enter the hash file (SHA256) you want to test")
+    # Input for user to upload a file
+    uploaded_file = st.file_uploader("Upload a file")
 
-    # Check if the SHA256 hash is provided
-    if sha256_hash:
+    # Check if a file is uploaded
+    if uploaded_file:
+        # Calculate SHA256 hash of the uploaded file
+        sha256_hash = hashlib.sha256(uploaded_file.read()).hexdigest()
+
+        st.subheader("SHA256 Hash:")
+        st.write(sha256_hash)
+
+        # Get the report for the SHA256 hash
         file_report = connection.get_file_report(sha256_hash)
 
         st.subheader("File Report:")
         st.write(pd.DataFrame.from_dict(file_report))
     else:
-        st.warning("Please enter a SHA256 hash.")
+        st.warning("Please upload a file.")
 else:
     st.warning("Please enter your VirusTotal API key.")
