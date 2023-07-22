e239c2a030770f4e7351dc9287f0c0eb539e58c9
Nadhiar Pradana <nadhiar11@gmail.com>
2023-07-22 21:02:31 +0700

    Edit uri

--- app.py
+++ app.py
@@ -11,18 +11,17 @@ API_KEY = st.text_input("Enter your VirusTotal API key", type="password")
 if API_KEY:
     connection = VirusTotalConnection(API_KEY)
 
-    # Test the connection with a SHA256 hash
-    sha256_hash = "8c52bb3c67a0de4c0f1590057b2a864158e30aa9c717eaa01a32b1cf8ae9bbfd"
-    file_report = connection.get_file_report(sha256_hash)
+    # Input for user to enter the URL
+    url = st.text_input("Enter the URL you want to test")
 
-    # Test the connection with a URL
-    url = "YOUR_URL"
-    url_report = connection.get_url_report(url)
+    # Check if the URL is provided
+    if url:
+        url_report = connection.get_url_report(url)
 
-    st.subheader("File Report:")
-    st.write(pd.DataFrame.from_dict(file_report))
-
-    st.subheader("URL Report:")
-    st.write(pd.DataFrame.from_dict(url_report))
+        st.subheader("URL Report:")
+        st.write(pd.DataFrame.from_dict(url_report))
+    else:
+        st.warning("Please enter a URL.")
 else:
     st.warning("Please enter your VirusTotal API key.")
+
